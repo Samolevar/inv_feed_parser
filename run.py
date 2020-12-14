f76b7398fc8e97d808ba87e852cf511e7feff599
Denis Aminev <denis.aminev@softwareone.com>
2020-12-14 09:46:37 +0500

    trying another time interval

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
+import logging
+
 from apscheduler.schedulers.blocking import BlockingScheduler
 from bot import bot_channel_updater
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', hours=3)
+@sched.scheduled_job('interval', minutes=1)
 def timed_job():
+    logger.info("Update news")
     bot_channel_updater.update()
 
 
